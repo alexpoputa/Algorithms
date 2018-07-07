53a6f314cf552192ac2de52239cb36e55f4c906d
Poputa Alexandru <33935806+PAAlex01@users.noreply.github.com>
2018-07-07 21:20:54 +0300

    Update index.js

--- Exercises/steps/index.js
+++ Exercises/steps/index.js
@@ -17,6 +17,37 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+  function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  const add = stair.length <= row ? '#' : ' ';
+  steps(n, row, stair + add);
+}
+
+//SOLUTION 2:
+
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+//
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//
+//     console.log(stair);
+//   }
+// }
+}
 
 module.exports = steps;
