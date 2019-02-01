086f17eba19318782a68db8261108c803a4f1440
Poputa Alexandru <33935806+alexpoputa@users.noreply.github.com>
2019-02-01 13:19:30 +0200

    Create index.js

---
+++ Exercises/removeExclamationMarks/index.js
@@ -0,0 +1,6 @@
+function removeExclamationMarks(s) {
+  // Create a var that removes exclamation marks with regex
+  var regex = /!/g;
+  // Return a new string with those exclamation marks replaced
+  return s.replace(regex, '');
+}
