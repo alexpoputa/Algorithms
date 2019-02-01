daa29c19153b55bd1af80e8c137cf85cb0a71374
Poputa Alexandru <33935806+alexpoputa@users.noreply.github.com>
2019-02-01 13:45:43 +0200

    Create index.js

---
+++ Exercises/oddEven/index.js
@@ -0,0 +1,11 @@
+function oddOrEven(array) {
+   var result = 0;
+   for(var i=0;i<array.length;i++){
+     result += array[i];
+   }
+   if(result % 2 == 0){
+     return 'even';
+   }else {
+     return 'odd';
+   }
+}
