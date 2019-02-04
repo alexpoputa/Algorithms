3717766e1690bab6127c0890d28e47e8f4374ff6
Poputa Alexandru <33935806+alexpoputa@users.noreply.github.com>
2019-02-04 14:18:20 +0200

    remove exclamation marks in a string

--- Exercises/removeExclamationMarks/index.js
+++ Exercises/removeExclamationMarks/index.js
@@ -1,6 +1,6 @@
 function removeExclamationMarks(s) {
-  // Create a var that removes exclamation marks with regex
+   // Create a var that removes exclamation marks with regex
   var regex = /!/g;
-  // Return a new string with those exclamation marks replaced
+   // Return a new string with those exclamation marks replaced
   return s.replace(regex, '');
 }
