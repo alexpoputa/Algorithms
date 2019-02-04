9a18ef56fb953137f772a8beeac5d65dee4bf2a5
Poputa Alexandru <33935806+alexpoputa@users.noreply.github.com>
2019-02-04 19:47:16 +0200

    Update index.js

--- Exercises/abreviateWords/index.js
+++ Exercises/abreviateWords/index.js
@@ -1,8 +1,8 @@
 function abbrevName(name){
    var x = name
-   // Split the words from string join it to an array to loop
+   // Split the words from string and join it to an array to loop
    .split(' ')
-   // Loop throught each word in the string
+   // Loop throught each word in the array
    .map(a => 
    // Capitalize first letter of each word, add a dot after it and join it back to string
    a.charAt(0).toUpperCase() + ".").join('');
