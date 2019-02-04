004915ea41d0dbcc68354b4e6bd24479e7cbcbdb
Poputa Alexandru <33935806+alexpoputa@users.noreply.github.com>
2019-02-04 19:46:27 +0200

    abreviate words and return first letter of each

---
+++ Exercises/abreviateWords/index.js
@@ -0,0 +1,11 @@
+function abbrevName(name){
+   var x = name
+   // Split the words from string join it to an array to loop
+   .split(' ')
+   // Loop throught each word in the string
+   .map(a => 
+   // Capitalize first letter of each word, add a dot after it and join it back to string
+   a.charAt(0).toUpperCase() + ".").join('');
+   // Return the string without the last element of it, which is a dot
+   return x.slice(0,-1);
+}
