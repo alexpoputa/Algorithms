bd8a4065294970cf45cc27ddb0d3721d74c6fce4
Poputa Alexandru <33935806+alexpoputa@users.noreply.github.com>
2019-02-10 15:37:40 +0200

    Create index.js

---
+++ Exercises/removeArray/index.js
@@ -0,0 +1,11 @@
+/*
+Write a function, gooseFilter/goose_filter/GooseFilter, that takes an array of strings 
+as an argument and returns a filtered array containing the same elements but with the 'geese' removed.
+*/
+
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  
+  // Return birds words if those words are not included in geese array
+ return birds.filter(b => !geese.includes(b));
+};
