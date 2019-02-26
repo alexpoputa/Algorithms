8e973396850e9c38e42090f8f572a24e66f80a7a
Poputa Alexandru <33935806+alexpoputa@users.noreply.github.com>
2019-02-26 17:02:36 +0200

    Update index.js

--- Exercises/ideasNumber/index.js
+++ Exercises/ideasNumber/index.js
@@ -1,7 +1,7 @@
-For every good kata idea there seem to be quite a few bad ones!
+/*For every good kata idea there seem to be quite a few bad ones!
 In this exercise you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. 
 If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. 
-If there are no good ideas, as is often the case, return 'Fail!'.
+If there are no good ideas, as is often the case, return 'Fail!'.*/
 
 function well(x){
  var good = 0;
