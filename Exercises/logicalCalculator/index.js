832994c118be167022f1b3b904e7d1d2c6cea975
Poputa Alexandru <33935806+alexpoputa@users.noreply.github.com>
2019-03-03 16:50:15 +0200

    Update index.js

--- Exercises/logicalCalculator/index.js
+++ Exercises/logicalCalculator/index.js
@@ -1,3 +1,24 @@
+
+function logicalCalc(array, op){
+  var result = array[0];
+  for(var i = 1; i < array.length; i++)
+  {
+    if(op == "AND")
+    {
+      result = result && array[i];
+    }
+    if(op == "OR")
+    {
+      result = result || array[i];
+    }
+    if(op == "XOR")
+    {
+      result = result != array[i];
+    }
+  }
+  return result;
+}
+
 /* 
 Test.describe('Basic tests',_=>{
 Test.assertEquals(logicalCalc([true, true, true, false], "AND"), false);
@@ -25,22 +46,3 @@ Test.assertEquals(logicalCalc([true], "XOR"), true);
 */
 
 
-function logicalCalc(array, op){
-  var result = array[0];
-  for(var i = 1; i < array.length; i++)
-  {
-    if(op == "AND")
-    {
-      result = result && array[i];
-    }
-    if(op == "OR")
-    {
-      result = result || array[i];
-    }
-    if(op == "XOR")
-    {
-      result = result != array[i];
-    }
-  }
-  return result;
-}
